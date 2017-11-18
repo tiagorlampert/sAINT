5d5a2dd3f4cef7c11c17f47bfa32b13fad077e12
Tiago Rodrigo Lampert <tiagorlampert@gmail.com>
2017-11-18 13:44:03 -0500

    Add files via upload

---
+++ content/UNINSTALL.bat
@@ -0,0 +1,12 @@
+:: Kill java process
+taskkill /f /im javaw.exe
+
+:: Delete (s)AINT folder
+rmdir /s /q %appdata%\(s)AINT
+
+:: Delete entry registry
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v Security /f
+
+cls
+echo "[*] OK"
+pause
